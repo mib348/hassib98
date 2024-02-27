a769596dab9e06cce2f90d1d67ca87676d055af7
mib348 <ibrahimbutt348@gmail.com>
2024-02-27 21:09:10 +0500

    fetching order number from order api on checkout

--- checkout-callback-script.js
+++ checkout-callback-script.js
@@ -9,42 +9,53 @@ sessionStorage.clear();
 </script>
 
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    var newSection = document.createElement('div');
-    newSection.className = 'custom-section';
-    newSection.style.display = 'flex';
-    newSection.style.marginTop = '0px'; // Adjust spacing as needed
-    newSection.style.borderTop = '1px solid lightgray'; // Adjust spacing as needed
-    newSection.style.paddingTop = '30px'; // Adjust spacing as needed
+document.addEventListener('DOMContentLoaded', async function() {
+    const url = 'https://app.sushi.catering/api/getordernumber/' + Shopify.checkout.order_id;
 
-    var qrCodeContainer = document.createElement('div');
-    qrCodeContainer.id = 'order-qr-code';
-    qrCodeContainer.style.flex = '1';
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const order_number = data.toString();
+        console.log(`The order number is: ${order_number}`);
+    
+        var newSection = document.createElement('div');
+        newSection.className = 'custom-section';
+        newSection.style.display = 'flex';
+        newSection.style.marginTop = '0px'; 
+        newSection.style.borderTop = '1px solid lightgray'; 
+        newSection.style.paddingTop = '30px';
 
-    var messageContainer = document.createElement('div');
-    messageContainer.className = 'custom-message';
-    messageContainer.style.flex = '1';
-    messageContainer.style.paddingLeft = '20px'; // Adjust spacing as needed
-    messageContainer.innerHTML = '<p>Deinen QR-Code erhältst du per Mail. Bitte scanne deinen QR-Code am Gerät und entnehme deine Bestellung.</p>'; // Customize your message
+        var qrCodeContainer = document.createElement('div');
+        qrCodeContainer.id = 'order-qr-code';
+        qrCodeContainer.style.flex = '1';
 
-    newSection.appendChild(qrCodeContainer);
-    newSection.appendChild(messageContainer);
+        var messageContainer = document.createElement('div');
+        messageContainer.className = 'custom-message';
+        messageContainer.style.flex = '1';
+        messageContainer.style.paddingLeft = '20px'; 
+        messageContainer.innerHTML = '<p>Deinen QR-Code erhältst du per Mail. Bitte scanne deinen QR-Code am Gerät und entnehme deine Bestellung.</p>'; 
 
-    var orderSummary = document.querySelector('.order-summary__sections');
-    if (orderSummary) {
-        orderSummary.appendChild(newSection);
+        newSection.appendChild(qrCodeContainer);
+        newSection.appendChild(messageContainer);
 
-        var order_number = '{{ order.name }}';
-        order_number = order_number.replace('#', '');
-        new QRCode(document.getElementById("order-qr-code"), {
-            text: order_number,
-            width: 170,
-            height: 170,
-            colorDark : "#000000",
-            colorLight : "#ffffff",
-            correctLevel : QRCode.CorrectLevel.H
-        });
+        var orderSummary = document.querySelector('.order-summary__sections');
+        if (orderSummary) {
+            orderSummary.appendChild(newSection);
+
+            // order_number = order_number.replace('#', '');
+
+            new QRCode(document.getElementById("order-qr-code"), {
+                text: order_number, 
+                width: 170,
+                height: 170,
+                colorDark : "#000000",
+                colorLight : "#ffffff",
+                correctLevel : QRCode.CorrectLevel.H
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return; 
     }
 });
 </script>
-
