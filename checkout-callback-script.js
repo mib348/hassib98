44506d568fe703855d33dfb33622889ee0f887d3
mib348 <ibrahimbutt348@gmail.com>
2024-03-13 16:19:22 +0500

    The max_quantity value will be displayed at the end on every item on the success page. Can you please not show that because some clients maybe think this is the amount they bought. Please hide that information there.

--- checkout-callback-script.js
+++ checkout-callback-script.js
@@ -1,3 +1,7 @@
+<style>
+.product__description span:nth-child(3){display:none !important;}
+</style>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js" integrity="sha512-CNgIRecGo7nphbeZ04Sc13ka07paqdeTu0WR1IM4kNcpmBAUSHSQX0FslNhTDadL4O5SAGapGt4FodqL8My0mA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script>
@@ -37,7 +41,7 @@ function getContent(mql){
         return orderContent = '<div style="display: flex; flex-direction: column; align-content: center; justify-content: center; align-items: center;"><div id="order-qr-code" style="flex: 1;margin-bottom: 15px;"></div><div class="custom-message" style="flex: 1; padding-left: 20px;"><p>Bitte scanne deinen QR-Code am Gerät und entnehme deine Bestellung. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail. Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/">hier</a>.</p></div></div>';
     }
     else
-        return orderContent = '<div style="display: flex;"><div id="order-qr-code" style="flex: 1;"></div><div class="custom-message" style="flex: 1; padding-left: 20px;"><p>Bitte scanne deinen QR-Code am Gerät und entnehme deine Bestellung. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail. Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/">hier</a>.</p></div></div>';
+        return orderContent = '<div style="display: flex;"><div id="order-qr-code" style="flex: 1;"></div><div class="custom-message" style="flex: 1; padding-left: 20px;"><p>Wenn du keinen QR-Code siehst, lade diese Seite neu. Scanne deinen QR-Code an der Station, um deine Artikel zu entnehmen. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail. Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/pages/faq">hier</a>.</p></div></div>';
 }
 
 getOrderNumber();
