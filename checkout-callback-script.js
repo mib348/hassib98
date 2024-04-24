a53a5eabc1fcf47ffc22a1c78f9f11e68bc88114
mib348 <ibrahimbutt348@gmail.com>
2024-04-24 19:29:16 +0500

    make the size of the qr-code which is displayed after order success bigger? It should fill the box.

--- checkout-callback-script.js
+++ checkout-callback-script.js
@@ -1,6 +1,16 @@
 <style>
 .product__description span:nth-child(3) { display: none !important; }
 .content-box, .step__footer__continue-btn, #loadingMessage { display: none; }
+@media (max-width: 480px) {
+    #order-qr-code {
+        width: 100%;
+        max-width: none;
+    }
+    #order-qr-code img{
+        width: 100%;
+        height:auto;
+    }
+}
 </style>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js" integrity="sha512-CNgIRecGo7nphbeZ04Sc13ka07paqdeTu0WR1IM4kNcpmBAUSHSQX0FslNhTDadL4O5SAGapGt4FodqL8My0mA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -43,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function getContent(mql){
         if (mql.matches) {
-            return orderContent = '<div style="display: flex; flex-direction: column; align-content: center; justify-content: center; align-items: center;"><div id="order-qr-code" style="flex: 1;margin-bottom: 15px;"></div><div class="custom-message" style="flex: 1; padding-left: 20px;"><p>Scanne deinen QR-Code an der Station, um deine Artikel zu entnehmen. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail. Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/pages/faq">hier</a>.</p></div></div>';
+            return orderContent = '<div style="display: flex; flex-direction: column; align-content: center; justify-content: center; align-items: center;"><div id="order-qr-code" style="flex: 1;margin-bottom: 15px;"></div><div class="custom-message" style="flex: 1;"><p>Scanne deinen QR-Code an der Station, um deine Artikel zu entnehmen. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail. Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/pages/faq">hier</a>.</p></div></div>';
         }
         else
             return orderContent = '<div style="display: flex;"><div id="order-qr-code" style="flex: 1;"></div><div class="custom-message" style="flex: 1; padding-left: 20px;"><p>Scanne deinen QR-Code an der Station, um deine Artikel zu entnehmen. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail. Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/pages/faq">hier</a>.</p></div></div>';
