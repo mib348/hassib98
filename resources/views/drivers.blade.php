6255fb0007346d7dc030c010b9cc125177269ac1
Ibrahim Butt <ibrahimbutt348@gmail.com>
2025-01-26 21:39:59 +0500

    is the textfield html input workable? it works now, but I want to use <br>, <b> etc. Use Blade syntax to render location address as HTML in drivers view

--- resources/views/drivers.blade.php
+++ resources/views/drivers.blade.php
@@ -53,7 +53,7 @@
 
                         <div id="collapse{{ $loop->index }}" class="accordion-collapse collapse @if($loop->first) show @endif" aria-labelledby="heading{{ $loop->index }}" data-bs-parent="#accordion">
                             <div class="accordion-body">
-                                <p>{{ $arrProducts['location_data']['address'] }}</p>
+                                <p>{!! $arrProducts['location_data']['address'] !!}</p>
 
                                 <h6>IMMEDIATE INVENTORY</h6>
                                 <div class="row">
