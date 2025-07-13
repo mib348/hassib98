1b7dc00f25342c14dd2dea8b617d617d98e7fd9c
Your Name <your-email@example.com>
2025-07-14 04:31:45 +0500

    The time is not updating after midnight. It still says delivery arrived today even though it is after midnight and the next day has started
    Update fulfillment time retrieval to use Carbon for accurate timezone handling in ShopifyController

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -901,7 +901,7 @@ class ShopifyController extends Controller
         if ($location) {
             // If the parameter is passed, select all fields for the specified location
             $arrLocations = Locations::where('name', $location)->first();
-            $driverFulfilledStatus = DriverFulfilledStatus::select('created_at')->where('location', $location)->where('date', date('Y-m-d'))->latest()->first();
+            $driverFulfilledStatus = DriverFulfilledStatus::select('created_at')->where('location', $location)->where('date', Carbon::now('Europe/Berlin')->format('Y-m-d'))->latest()->first();
             $arrLocations['driver_fulfillment_time'] = $driverFulfilledStatus ? "erfolgt um " . Carbon::parse($driverFulfilledStatus->created_at, 'Europe/Berlin')->format('H:i') . " Uhr" : null;
 
             // if($location == "Delivery"){
