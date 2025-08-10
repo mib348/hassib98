bb1a218c0ed7abcb8f4166d1f5600caefdebc227
Your Name <your-email@example.com>
2025-08-11 01:01:03 +0500

    Add checkout additional scripts for QR code generation and order handling
    This commit introduces a new script that enhances the checkout process by generating a QR code for order retrieval. It includes responsive design adjustments for mobile devices, loading indicators, and improved visibility of order instructions based on the delivery location. The script fetches the order number from an API and displays relevant messages to the user, ensuring a smoother user experience during the checkout process.

---
+++ snippets/checkout-additional-scripts-backup.js
@@ -0,0 +1,147 @@
+<style>
+.product__description__property:not(:nth-child(4)):not(:nth-child(5)):not(:nth-child(6)) {
+    display: none !important;
+}
+.content-box, .step__footer__continue-btn, #loadingMessage { display: none; }
+
+@media (max-width: 480px) {
+    #order-qr-code {
+        width: 100%;
+        max-width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    #order-qr-code img {
+        width: 100%;
+        height: auto;
+    }
+}
+</style>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js" integrity="sha512-CNgIRecGo7nphbeZ04Sc13ka07paqdeTu0WR1IM4kNcpmBAUSHSQX0FslNhTDadL4O5SAGapGt4FodqL8My0mA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+    sessionStorage.clear();
+
+    async function getOrderNumber() {
+        showLoadingMessage(true); // Show loading message before fetching the order number
+
+        try {
+            const url = 'https://dev.sushi.catering/api/getordernumber/' + Shopify.checkout.order_id;
+            const response = await fetch(url);
+            const data = await response.json();
+            const order_number = data.order_number.toString();
+            console.log(`The order number is: ${order_number}`);
+            const stationFlag = data.arrLocation.no_station;
+            console.log(`The no_station is: ${stationFlag}`);
+
+            if (stationFlag === 'Y' || data.arrLocation.name == 'Delivery') { 
+                displayStationInstructions(true, data.arrLocation); // No QR code, show alternate text 
+            }
+            else{
+                const mobileQuery = window.matchMedia("(max-width: 480px)");
+                const orderContent = getContent(mobileQuery);
+                Shopify.Checkout.OrderStatus.addContentBox(orderContent);
+    
+                new QRCode(document.getElementById("order-qr-code"), {
+                    text: order_number,
+                    width: mobileQuery.matches ? 350 : 180, // Adjust width based on screen size
+                    height: mobileQuery.matches ? 350 : 180, // Adjust height based on screen size
+                    colorDark : "#000000",
+                    colorLight : "#ffffff",
+                    correctLevel : QRCode.CorrectLevel.H
+                });
+    
+                toggleVisibility(true);
+            }
+
+        } catch (error) {
+            console.error(error);
+            toggleVisibility(false);
+        } finally {
+            showLoadingMessage(false); // Hide loading message after QR code is generated or in case of error
+        }
+    }
+
+    function getContent(mql) {
+        if (mql.matches) {
+            return `
+                <div style="width:100%;max-width:100%;display: flex; flex-direction: column; align-content: center; justify-content: center; align-items: center;">
+                    <div id="order-qr-code" style="flex: 1; margin-bottom: 15px;width:100%;"></div>
+                    <div class="custom-message" style="flex: 1;">
+                        <p>
+                            Scanne deinen QR-Code an der Station, um deine Artikel zu entnehmen. Stelle hierfür die maximale Helligkeit deines Mobilgeräts ein und halte dein Mobilgerät senkrecht, mittig und im Abstand von ca. 10cm vor den Scanner.
+                            Bitte achte darauf, dass der QR-Code die volle Breite deines Bildschirms ausfüllen muss. Falls der QR-Code im Browser zu klein angezeigt wird, kannst du hineinzoomen oder den QR-Code aus deiner E-Mail verwenden.
+                            Nach dem Scannen, folge den Anweisungen auf dem Monitor. Wenn deine Bestellung nicht gefunden wurde, versuche es nach 30 Sekunden erneut. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail.
+                            Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/pages/faq">hier</a>.
+                        </p>
+                    </div>
+                </div>
+            `;
+        } else {
+            return `
+                <div style="display: flex;">
+                    <div id="order-qr-code" style="flex: 1;"></div>
+                    <div class="custom-message" style="flex: 1; padding-left: 20px;">
+                        <p>
+                            Scanne deinen QR-Code an der Station, um deine Artikel zu entnehmen. Stelle hierfür die maximale Helligkeit deines Mobilgeräts ein und halte dein Mobilgerät senkrecht, mittig und im Abstand von ca. 10cm vor den Scanner.
+                            Bitte achte darauf, dass der QR-Code die volle Breite deines Bildschirms ausfüllen muss. Falls der QR-Code im Browser zu klein angezeigt wird, kannst du hineinzoomen oder den QR-Code aus deiner E-Mail verwenden.
+                            Nach dem Scannen, folge den Anweisungen auf dem Monitor. Wenn deine Bestellung nicht gefunden wurde, versuche es nach 30 Sekunden erneut. Deinen QR-Code findest du auch in deiner Bestellbestätigungsmail.
+                            Fragen und Antworten rund um deine Bestellung findest du <a href="https://sushi.catering/pages/faq">hier</a>.
+                        </p>
+                    </div>
+                </div>
+            `;
+        }
+    }
+
+    function displayStationInstructions(showQrCode, arrLocation) {
+        const contentBox = document.createElement('div');
+        contentBox.style.display = 'block';
+        contentBox.innerHTML = `
+            <p>
+                Wir liefern alle bestellten Gerichte in einer Sammellieferung täglich und frisch an die Rezeption. Achten Sie bitte darauf, dass die Gerichte bis zum Verzehr kühl gehalten werden müssen, z.B. im Kühlschrank. Um wie viel Uhr wir genau anliefern, entnehmen Sie bitte der Seite auf die Sie weitergeleitet werden, nachdem Sie den Standort festgelegt haben.
+            </p>
+        `;
+        if(arrLocation.name == 'Delivery'){
+            contentBox.innerHTML = `
+                <p style="font-weight:bold;">
+                    ` + arrLocation.checkout_note + `
+                </p>
+            `;
+        }
+        Shopify.Checkout.OrderStatus.addContentBox(contentBox);
+        toggleVisibility(showQrCode);
+    }
+
+    function toggleVisibility(show) {
+        document.querySelectorAll('.content-box, .step__footer__continue-btn').forEach(el => {
+            el.style.display = show ? 'block' : 'none';
+        });
+    }
+
+    function showLoadingMessage(show) {
+        const loadingMessageElement = document.getElementById('loadingMessage') || createLoadingMessage();
+        loadingMessageElement.style.display = show ? 'block' : 'none';
+    }
+
+    function createLoadingMessage() {
+        const loadingMessageElement = document.createElement('div');
+        loadingMessageElement.id = 'loadingMessage';
+        loadingMessageElement.innerText = 'Details werden geladen. Bitte warten!...';
+        loadingMessageElement.style.position = 'fixed';
+        loadingMessageElement.style.top = '55%';
+        loadingMessageElement.style.left = '30%';
+        loadingMessageElement.style.transform = 'translate(-50%, -50%)';
+        loadingMessageElement.style.backgroundColor = '#fff';
+        loadingMessageElement.style.padding = '20px';
+        loadingMessageElement.style.borderRadius = '5px';
+        loadingMessageElement.style.boxShadow = '0 0 10px rgba(0,0,0,0.5)';
+        document.body.appendChild(loadingMessageElement);
+        return loadingMessageElement;
+    }
+
+    getOrderNumber();
+});
+</script>
